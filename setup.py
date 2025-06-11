13ec8f82a3ce39b45c11745980ef53481caa1d04
Cadu Zanin <caduzanin123@gmail.com>
2025-06-11 20:42:40 -0300

    Finalmente executavel funcionando

--- setup.py
+++ setup.py
@@ -1,48 +1,12 @@
 import cx_Freeze
-import sys
-import os
-from cx_Freeze import setup, Executable
-
-# Configuração especial para garantir a execução
-if sys.platform == "win32":
-    base = "Win32GUI"
-    # Isso força a criação do manifest
-    additional_options = {
-        "build_exe": {
-            "include_msvcr": True,
-            "silent": True
-        }
-    }
-else:
-    base = None
-    additional_options = {}
-
-# Configuração dos arquivos
-def get_all_files():
-    file_list = []
-    for root, _, files in os.walk("recursos"):
-        for file in files:
-            file_list.append(os.path.join(root, file))
-    return file_list
-
-setup(
-    name="Atitus Runner",
-    version="1.0",
-    description="Jogo de Corrida",
+executaveis = [ 
+               cx_Freeze.Executable(script="main.py", icon="recursos/icon.ico") ]
+cx_Freeze.setup(
+    name = "AtitusRunner",
     options={
-        "build_exe": {
-            "packages": ["pygame", "pyttsx3","encodings"],
-            "includes": ["pygame._sdl2"],
-            "include_files": get_all_files(),
-            "optimize": 2
-        },
-        **additional_options
-    },
-    executables=[
-        Executable(
-            "main.py",
-            base=base,
-            icon="recursos/icon.ico",
-            target_name="AtitusRunner.exe",
-            # Configurações extras para Windows
-            uac_admin=True if sys.platform == "win32" else False)])
+        "build_exe":{
+            "packages":["pygame", "pyttsx3"],
+            "include_files":["recursos"]
+        }
+    }, executables = executaveis
+)
